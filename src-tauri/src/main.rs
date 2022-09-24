2a5f377986552148d5a1d200c2eaf569fe915e16
Michael Skrzypietz <michael.skr97@gmail.com>
2022-09-24 18:19:53 +0200

    add basic logic to connect to twitch irc and read messages for a channel

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,35 @@
-#![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
-)]
+use twitch_irc::login::StaticLoginCredentials;
+use twitch_irc::message::ServerMessage;
+use twitch_irc::{ClientConfig, SecureTCPTransport, TwitchIRCClient};
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+
+    let config = ClientConfig::default();
+    let (mut incoming_messages, client) =
+        TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
+
+    let join_handle = tokio::spawn(async move {
+        while let Some(message) = incoming_messages.recv().await {
+            if let ServerMessage::Privmsg(message) = message {
+                println!(
+                    "Received from {}: {}",
+                    message.sender.name, message.message_text
+                );
+            }
+        }
+    });
+
+    let channel = std::env::var("CHANNEL").unwrap();
+    client.join(channel).unwrap();
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![hello])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+
+    join_handle.await.unwrap();
 }
 
 #[tauri::command]
