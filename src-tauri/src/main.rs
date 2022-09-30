57effa300748717f2dd21314073f2b10c9889cd8
Michael Skrzypietz <michael.skr97@gmail.com>
2022-10-01 01:17:18 +0200

    clean up the rust code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,5 @@
 use serde::Serialize;
 use tauri::{Manager, RunEvent};
-use tokio::sync::mpsc;
 use twitch_irc::login::StaticLoginCredentials;
 use twitch_irc::message::ServerMessage;
 use twitch_irc::{ClientConfig, SecureTCPTransport, TwitchIRCClient};
@@ -15,38 +14,34 @@ struct Payload {
 async fn main() {
     dotenv::dotenv().ok();
 
-    let (tx, mut rx) = mpsc::channel(32);
-
     let config = ClientConfig::default();
     let (mut incoming_messages, client) =
         TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
-
-    let join_handle = tokio::spawn(async move {
-        while let Some(message) = incoming_messages.recv().await {
-            if let ServerMessage::Privmsg(message) = message {
-                tx.send(Payload {
-                    username: message.sender.name,
-                    content: message.message_text,
-                })
-                .await
-                .unwrap();
-            }
-        }
-    });
 
     let channel = std::env::var("CHANNEL").unwrap();
     client.join(channel).unwrap();
 
     let app = tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![hello])
+        .invoke_handler(tauri::generate_handler![])
         .build(tauri::generate_context!())
         .expect("error while running tauri application");
 
     let app_handle = app.app_handle();
     tokio::spawn(async move {
-        while let Some(payload) = rx.recv().await {
-            println!("Emitting => {}: {}", payload.username, payload.content);
-            app_handle.emit_all("on_message", payload).unwrap();
+        while let Some(message) = incoming_messages.recv().await {
+            if let ServerMessage::Privmsg(message) = message {
+                let username = message.sender.name;
+                let content = message.message_text;
+                let payload = Payload {
+                    username: username.clone(),
+                    content: content.clone(),
+                };
+
+                match app_handle.emit_all("on_message", payload) {
+                    Ok(_) => println!("emitting => {}: {}", username, content),
+                    Err(err) => println!("error while emitting a message: {}", err),
+                };
+            }
         }
     });
 
@@ -64,15 +59,4 @@ async fn main() {
             app_handler.exit(0);
         }
     });
-
-    join_handle.await.unwrap();
-}
-
-#[tauri::command]
-fn hello(name: &str) -> Result<String, String> {
-    if name.contains(' ') {
-        Err("Name shoudl not contain spaces".to_string())
-    } else {
-        Ok(format!("Hello, {}", name))
-    }
 }
